ffcaa945062cee7ffdc981065b0662823743875e
suprim12 <suprimofficial@yahoo.com>
2021-02-17 14:15:39 +0545

    404 Test

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php include('./config.php');?>
+<?php include('./data.php');?>
+
+<?php include(ROOT_PATH.'/includes/header.php');?>
+
+
+<section class="site-content">
+  <h1>404</h1>
+  <a href="index.php">Go Home</a>
+</section>
+
+<?php include(ROOT_PATH.'/includes/footer.php');?>
