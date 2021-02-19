0ac88f2902ac43a903da03d3bff30e586e968e98
suprim12 <suprimofficial@yahoo.com>
2021-02-19 10:42:22 +0545

    An

--- includes/footer.php
+++ includes/footer.php
@@ -88,9 +88,11 @@
   <script
     defer
     src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    crossorigin="anonymous"
   ></script>
   <script
     defer
+    crossorigin="anonymous"
     src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
   ></script>
   <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script> -->
